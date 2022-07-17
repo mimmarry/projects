fef74a26ea3b38a4f5136267d63c7a7328a6e0aa
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-07-18 02:47:53 +0300

    Add files via upload

---
+++ 33.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 10;
+        string c = "7 см";
+        Console.WriteLine($"{a}  {b} \n{c}");
+    }
+}
